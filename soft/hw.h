7b9c3b0503cd71fc76448078825d382410bf2a9e
Ivan Zagorodny <ivanzagorodny2003@gmail.com>
2023-08-24 16:56:48 -0300

    first commit

---
+++ soft/hw.h
@@ -0,0 +1,15 @@
+#ifndef _HW_H
+#define _HW_H
+//=============================================================================
+// Definicion 
+#define LCD_DB4   PIN_D3
+#define LCD_DB5   PIN_D2
+#define LCD_DB6   PIN_D1
+#define LCD_DB7   PIN_D0
+//
+#define LCD_RS    PIN_C0
+#define LCD_RW    PIN_C1
+#define LCD_E     PIN_C2
+
+//=============================================================================
+#endif
